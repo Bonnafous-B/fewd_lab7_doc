297e6a186b4b916b7908ef4a8bc348856afcc75b
Bonnafous-B <74108740+Bonnafous-B@users.noreply.github.com>
2020-11-07 17:38:55 +0000

    etienne Bonnafous Activity 7

---
+++ script.js
@@ -0,0 +1,30 @@
+var firstname = "etienne Bonnafous-Boucher";
+
+var favouritecolor = "green";
+
+var favouritefood = "lasagna";
+
+var favouritecity = "Boston";
+
+console.log(firstname);
+console.log(favouritecolor);
+console.log(favouritefood);
+console.log(favouritecity); //Console.log allows variable in the console
+
+
+
+var phrase = " A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase = "is what we see. Rainbow colours, are made with light bendingyou see!";
+var one = "Red";
+var two = "Orange";
+var three = "Yellow";
+var four = "Green";
+var five = "Blue";
+var six = "Indigo";
+var seven = "Violet";
+
+
+console.log(phrase+one+two+three+four+five+six+seven)
+
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabet.length);
